7608ebbcaf6de0145021f70e19f638ffaa1e2b70
Savil Srivastava <savil@cs.stanford.edu>
2018-12-18 20:43:24 -0800

    delete SimpleStorage contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var PermaURLStorage = artifacts.require("./PermaURLStorage.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
   deployer.deploy(PermaURLStorage);
 };
